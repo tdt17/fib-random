8c883947831d6b8e9eca5215fb2f544ed710a864
Torsten Hain <torsten.hain@spring-media.de>
2022-03-21 23:31:34 +0100

    add next button to reset all ready/estimates

--- meet/src/components/Result.vue
+++ meet/src/components/Result.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import { deleteField } from 'firebase/firestore';
 import { computed } from 'vue';
-import { allReady, activeUsers } from '../firestore'
+import { allReady, activeUsers, isAdmin, updateState, State, session, User } from '../firestore'
 import Dice from './Dice.vue';
 const values = computed(() => {
   const users = Object.values(activeUsers.value)
@@ -8,8 +9,17 @@ const values = computed(() => {
     values[estimate] = (values[estimate] ?? 0) + 1
     return values
   },{} as {[val: string]: number})
-})  
-
+})
+const next = () => {
+  const users: State['users'] = Object.fromEntries(
+    Object.keys(session.state?.users || {})
+      .map(name => [name, { 
+        ready: false, 
+        estimate: deleteField() as any
+      } as User])
+  )
+  updateState({ users })
+}
 </script>
 
 <template>
@@ -19,6 +29,7 @@ const values = computed(() => {
     <div>
       <span class="entry" v-for="(times, value) in values">{{times}}x {{value}}</span>
     </div>
+    <button v-if="isAdmin" @click="next">Next</button>
   </div>
 </template>
 
